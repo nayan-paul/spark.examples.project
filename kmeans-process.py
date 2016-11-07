0f2441dd9dff8e10cfe03bf6f92a7a238b168391
nayan-paul <nayan.j.paul@gmail.com>
2016-11-07 15:34:42 +0530

    7 nov 2016

---
+++ kmeans-process.py
@@ -0,0 +1,40 @@
+import traceback
+import numpy as np
+from pyspark import SparkContext
+from pyspark.mllib.clustering import KMeans
+
+#spark-submit --master local kmeans-process.py
+#spark-submit --master yarn-cluster kmeans-process.py 
+
+#M2
+def mapData(line):
+	try:
+		return np.array([float(x) for x in line.split(',')])
+	#try
+	except:
+		traceback.print_exc()
+	#except
+	finally:
+		print 'End of process...'
+	#finally
+#M2
+###############################################################################################################
+
+if __name__=="__main__":
+	context= SparkContext(appName='KMeans example')
+	try:
+		data = context.textFile('hdfs://10.0.215.232:8020/data/kmeans/kmeans-data.txt')
+		inputRDD = data.map(mapData)
+		
+		model = KMeans.train(inputRDD,2)
+		print "................................center " + str(model.clusterCenters)
+		print "................................cost "+str(model.computeCost(inputRDD))
+	#try
+	except:
+		traceback.print_exc()
+	#except
+	finally:
+		context.stop()
+		print 'End of Process...'
+	#finally
+#if
